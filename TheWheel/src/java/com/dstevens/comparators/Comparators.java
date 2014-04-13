1ab0a741799465dc71f5006a1a0ea71ebe18303a
Drew Stevens <wastevens@gmail.com>
2014-04-13 15:45:30 -0400

    Fixing the nullsafe comaprators.

--- TheWheel/src/java/com/dstevens/comparators/Comparators.java
+++ TheWheel/src/java/com/dstevens/comparators/Comparators.java
@@ -13,7 +13,7 @@ public class Comparators {
         };
     }
     
-    public static <E extends Comparable<E>> Comparator<E> nullSafe(final Comparator<E> comparator) {
+    public static <E extends Comparable<E>> Comparator<E> nullsLast(final Comparator<E> comparator) {
         return new Comparator<E>() {
             @Override
             public int compare(E e1, E e2) {
@@ -25,6 +25,24 @@ public class Comparators {
                 }
                 if (e2 == null) {
                     return -1;
+                }
+                return comparator.compare(e1, e2);
+            }
+        };
+    }
+    
+    public static <E extends Comparable<E>> Comparator<E> nullsFirst(final Comparator<E> comparator) {
+        return new Comparator<E>() {
+            @Override
+            public int compare(E e1, E e2) {
+                if (e1 == e2) {
+                    return 0;
+                }
+                if (e1 == null) {
+                    return -1;
+                }
+                if (e2 == null) {
+                    return 1;
                 }
                 return comparator.compare(e1, e2);
             }
--- TheWheel/src/test/com/dstevens/comparators/ComparatorsTest.java
+++ TheWheel/src/test/com/dstevens/comparators/ComparatorsTest.java
@@ -1,12 +1,22 @@
 package com.dstevens.comparators;
 
+import static com.dstevens.collections.Lists.list;
+import static org.junit.Assert.assertEquals;
+
+import java.util.*;
+
 import org.junit.Test;
 
 public class ComparatorsTest {
 
     @Test
     public void testComplexChainComparator() {
+        List<String> foo = list("c", null, "b", "d", "a", "e", null, "f");
+        Collections.sort(foo, Comparators.nullsLast(StringComparator.INSTANCE));
+        assertEquals(list("a", "b", "c", "d", "e", "f", null, null), foo);
         
+        Collections.sort(foo, Comparators.nullsFirst(StringComparator.INSTANCE));
+        assertEquals(list(null, null, "a", "b", "c", "d", "e", "f"), foo);
     }
     
 }
