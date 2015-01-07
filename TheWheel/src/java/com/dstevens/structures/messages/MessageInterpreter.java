6c9b2a5765b45fbe63d1c3542ec71a6de8bce0bf
dstevens <dstevens@us.ibm.com>
2015-01-07 14:51:56 -0500

    Eliminated Validator and Message and MessageInterpreter.  YAGNI violation; I ain't needed 'em yet.

--- TheWheel/src/java/com/dstevens/structures/messages/MessageInterpreter.java
+++
@@ -1,9 +0,0 @@
-package com.dstevens.structures.messages;
-
-import java.util.function.Function;
-
-public interface MessageInterpreter extends Function<Message, String> {
-
-    String apply(Message message);
-    
-}
